c8edcbbb3f49dc0e90828fbb1e363581bb376463
tcwofs <tcwofs@users.noreply.github.com>
2020-03-03 20:37:22 +0300

    Updates

---
+++ precache-manifest.c82c272c6b5f879b866ec865bbcd34c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873aa21f1c7ff2e1ed09ad6f66322162",
+    "url": "/crl-timer/index.html"
+  },
+  {
+    "revision": "c01d55aebdeb4b2424ef",
+    "url": "/crl-timer/static/css/main.a5069583.chunk.css"
+  },
+  {
+    "revision": "39399e9c3a480114e327",
+    "url": "/crl-timer/static/js/2.82bf5f55.chunk.js"
+  },
+  {
+    "revision": "263aea54cc69a35c9a00cd2811ee7e25",
+    "url": "/crl-timer/static/js/2.82bf5f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01d55aebdeb4b2424ef",
+    "url": "/crl-timer/static/js/main.6a6ff86e.chunk.js"
+  },
+  {
+    "revision": "582f8c91e4269cef36f9",
+    "url": "/crl-timer/static/js/runtime-main.55e2a735.js"
+  }
+]);
